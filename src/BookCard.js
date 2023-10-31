73062b6fded5cbb45220f94cfc4d74898a84abc6
Luqmaan Essop <luqmaan.essop@amazee.com>
2023-10-31 06:38:51 +0200

    refactor: improve option selection

--- src/BookCard.js
+++ src/BookCard.js
@@ -3,6 +3,39 @@ export const BookCard = ({book, handleUpdate}) => {
 
   const bookImage = book['imageLinks'] && book['imageLinks'].thumbnail ? book['imageLinks'].thumbnail : 'https://picsum.photos/id/870/200/300?grayscale&blur=2';
 
+  const shelves = [
+    {
+      id: '1',
+      value: 'moveTo',
+      disabled: true,
+      label: 'Move to...'
+    },
+    {
+      id: '2',
+      value: 'currentlyReading',
+      disabled: false,
+      label: 'Currently reading'
+    },
+    {
+      id: '3',
+      value: 'wantToRead',
+      disabled: false,
+      label: 'Want to read'
+    },
+    {
+      id: '4',
+      value: 'read',
+      disabled: false,
+      label: 'Read'
+    },
+    {
+      id: '5',
+      value: 'none',
+      disabled: false,
+      label: 'None'
+    },
+  ]
+
   return (
   <div className="book">
     <div className="book-top">
@@ -24,15 +57,11 @@ export const BookCard = ({book, handleUpdate}) => {
         <select value={book.shelf} onChange={(e) => {
           handleUpdate(book, e.target.value);
         }}>
-          <option value="moveTo" disabled>
-            Move to...
-          </option>
-          <option value="currentlyReading">
-            Currently Reading
-          </option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
+          {shelves.map((shelf) => (
+            <option key={shelf.id} value={shelf.value} disabled={shelf.disabled}>
+              {shelf.label}
+            </option>
+          ))}
         </select>
       </div>
     </div>
